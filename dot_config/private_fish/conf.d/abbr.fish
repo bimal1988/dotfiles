697b8352e57eba507ab45d8f50958ce3cae10356
Bimal <bimaleal@gmail.com>
2024-07-21 23:06:43 +0530

    Abbreviations added

---
+++ dot_config/private_fish/conf.d/abbr.fish
@@ -0,0 +1,15 @@
+abbr --add l lsd
+abbr --add ls lsd
+abbr --add la lsd -a
+abbr --add ll lsd -l
+abbr --add lla lsd -la
+abbr --add lt lsd --tree --depth 2
+abbr --add j z
+abbr --add gs git status
+abbr --add ga git add
+abbr --add gc git commit -m
+abbr --add gp git push
+abbr --add gpl git pull
+abbr --add vi nvim
+abbr -a L --position anywhere --set-cursor "% | less"
+abbr -a F --position anywhere --set-cursor "% | fzf"
